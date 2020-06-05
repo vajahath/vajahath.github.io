8f1e04ea849c2695f497bd606c88a5d04beb8b1c
Vajahath Ahmed <vajuoff.1@gmail.com>
2020-06-06 02:36:28 +0530

    initial commit

---
+++ dist/README.md
@@ -0,0 +1,2 @@
+# pages
+my github pages
---
+++ dist/gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require("gulp");
+const htmlMin = require("gulp-htmlmin");
+const del = require("del");
+const path = require("path");
+
+const dest = path.join(__dirname, "dist");
+
+function cleanDist() {
+  return del(dest);
+}
+
+function minifyHtml() {
+  // minify
+  // const htmlSrc = src.map((el) => path.join(__dirname, el, "index.html"));
+  const htmlSrc = ["**/*.html", "!dist/**", "!node_modules/**"];
+  return gulp
+    .src(htmlSrc)
+    .pipe(
+      htmlMin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        collapseInlineTagWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest(dest));
+}
+
+function copyEverythingExceptHTML() {
+  return gulp
+    .src(["**/*", "!**/*.html", "!dist/**", "!node_modules/**"])
+    .pipe(gulp.dest(dest));
+}
+
+exports.default = gulp.series(cleanDist, minifyHtml, copyEverythingExceptHTML);
